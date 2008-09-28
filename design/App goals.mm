9bb61923ce318236572ca8682710c5db91d2a8cb
Patrick van der Velde <petrikvandervelde@gmail.com>
2008-09-28 15:59:48 +1300

    Finished intro in user spec. Minor updates in other documents

--- design/App goals.mm
+++ design/App goals.mm
@@ -3,7 +3,7 @@
 <node COLOR="#000000" CREATED="1183316704515" ID="Freemind_Link_735189972" MODIFIED="1208973049890" TEXT="App goals">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1183316720781" ID="Freemind_Link_920364966" MODIFIED="1221715998937" POSITION="right" TEXT="Mission / purpose" VSHIFT="-90">
+<node COLOR="#0033ff" CREATED="1183316720781" HGAP="-1" ID="Freemind_Link_920364966" MODIFIED="1221956154140" POSITION="right" TEXT="Mission / purpose" VSHIFT="-106">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 <node COLOR="#00b439" CREATED="1183316738515" ID="Freemind_Link_815637427" MODIFIED="1221715998937" TEXT="To provide designers / engineers with a &#xa;design tool that helps them develop kick-ass &#xa;products by providing them with the right simulation tools" VSHIFT="-80">
@@ -37,13 +37,13 @@
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1183316727250" ID="Freemind_Link_216287726" MODIFIED="1221715998937" POSITION="left" TEXT="audience" VSHIFT="1">
+<node COLOR="#0033ff" CREATED="1183316727250" HGAP="7" ID="Freemind_Link_216287726" MODIFIED="1221956147359" POSITION="left" TEXT="audience" VSHIFT="78">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1183317043765" ID="Freemind_Link_1896803953" MODIFIED="1221715998937" TEXT="Designer / engineer" VSHIFT="-58">
+<node COLOR="#00b439" CREATED="1183317043765" HGAP="12" ID="Freemind_Link_1896803953" MODIFIED="1221956161968" TEXT="Designer / engineer" VSHIFT="-62">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1183317071328" HGAP="30" ID="Freemind_Link_1010978857" MODIFIED="1221803440953" TEXT="Should not need to know about numerics" VSHIFT="-44">
+<node COLOR="#990000" CREATED="1183317071328" HGAP="16" ID="Freemind_Link_1010978857" MODIFIED="1221956234453" TEXT="Should not need to know about numerics" VSHIFT="-22">
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1215427041546" ID="Freemind_Link_1652465356" MODIFIED="1215427188859" TEXT="Never ask data that can be derrived">
 <font NAME="SansSerif" SIZE="12"/>
@@ -51,7 +51,7 @@
 <node COLOR="#111111" CREATED="1183317168234" ID="Freemind_Link_1684482748" MODIFIED="1215427211406" TEXT="Hide numerics"/>
 <node COLOR="#111111" CREATED="1215427214843" ID="Freemind_Link_326373888" MODIFIED="1215427236359" TEXT="Only ask data that the user may&#xa;have (like physics info)"/>
 </node>
-<node COLOR="#990000" CREATED="1183317097703" HGAP="36" ID="Freemind_Link_1816566190" MODIFIED="1221803421078" TEXT="Itegrated UI" VSHIFT="-32">
+<node COLOR="#990000" CREATED="1183317097703" HGAP="29" ID="Freemind_Link_1816566190" MODIFIED="1221956208171" TEXT="Itegrated UI" VSHIFT="9">
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1215427369546" ID="_" MODIFIED="1215427376484" TEXT="Automatically assemble simulation"/>
 <node COLOR="#111111" CREATED="1215427382656" ID="Freemind_Link_1059575041" MODIFIED="1215427393437" TEXT="Provide templated tools"/>
@@ -59,16 +59,16 @@
 <node COLOR="#111111" CREATED="1221918438388" ID="ID_1555186491" MODIFIED="1221918445998" TEXT="Never ask meta-data"/>
 </node>
 </node>
-<node COLOR="#990000" CREATED="1221803098437" ID="ID_577978477" MODIFIED="1221803102375" TEXT="Wants to">
+<node COLOR="#990000" CREATED="1221803098437" HGAP="13" ID="ID_577978477" MODIFIED="1221956210671" TEXT="Wants to" VSHIFT="44">
 <font NAME="SansSerif" SIZE="14"/>
-<node COLOR="#111111" CREATED="1183317081109" ID="Freemind_Link_1182402262" MODIFIED="1221803192046" TEXT="Create new design" VSHIFT="-58">
+<node COLOR="#111111" CREATED="1183317081109" HGAP="15" ID="Freemind_Link_1182402262" MODIFIED="1221956221578" TEXT="Create new design" VSHIFT="-40">
 <font NAME="SansSerif" SIZE="12"/>
-<node COLOR="#111111" CREATED="1202083443093" ID="Freemind_Link_913926509" MODIFIED="1215427302671" TEXT="Quick initial design">
+<node COLOR="#111111" CREATED="1202083443093" HGAP="18" ID="Freemind_Link_913926509" MODIFIED="1221956226046" TEXT="Quick initial design" VSHIFT="-18">
 <font NAME="SansSerif" SIZE="12"/>
 <node COLOR="#111111" CREATED="1202083489046" ID="Freemind_Link_267535155" MODIFIED="1208973049859" TEXT="Design exploration"/>
 <node COLOR="#111111" CREATED="1202083498328" ID="Freemind_Link_1424784825" MODIFIED="1208973049875" TEXT="Design comparisons"/>
 </node>
-<node COLOR="#111111" CREATED="1202083464515" ID="Freemind_Link_93303290" MODIFIED="1215427336859" TEXT="High accuracy final design" VSHIFT="28">
+<node COLOR="#111111" CREATED="1202083464515" HGAP="21" ID="Freemind_Link_93303290" MODIFIED="1222192795343" TEXT="Known accuracy final design" VSHIFT="6">
 <font NAME="SansSerif" SIZE="12"/>
 <node COLOR="#111111" CREATED="1215515412593" ID="Freemind_Link_648499237" MODIFIED="1215515418734" TEXT="Needs error margins"/>
 </node>
@@ -76,25 +76,25 @@
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node COLOR="#111111" CREATED="1221803198734" ID="ID_1681954751" MODIFIED="1221803204609" TEXT="Improve existing design"/>
+<node COLOR="#111111" CREATED="1221803198734" HGAP="14" ID="ID_1681954751" MODIFIED="1221956218343" TEXT="Improve existing design" VSHIFT="-9"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1183316948171" HGAP="21" ID="Freemind_Link_1275555554" MODIFIED="1221803405515" TEXT="Developers" VSHIFT="71">
+<node COLOR="#00b439" CREATED="1183316948171" HGAP="16" ID="Freemind_Link_1275555554" MODIFIED="1221956204578" TEXT="Developers" VSHIFT="-86">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1202083357515" HGAP="35" ID="Freemind_Link_716380042" MODIFIED="1221803413359" TEXT="Component development" VSHIFT="-7">
+<node COLOR="#990000" CREATED="1202083357515" HGAP="19" ID="Freemind_Link_716380042" MODIFIED="1221956166140" TEXT="Component development" VSHIFT="-13">
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1215514928859" ID="Freemind_Link_1728874102" MODIFIED="1215514939250" TEXT="Easy to use API"/>
 <node COLOR="#111111" CREATED="1215514920046" ID="Freemind_Link_65359574" MODIFIED="1215514947875" TEXT="Easy verification"/>
 </node>
-<node COLOR="#990000" CREATED="1215515356484" HGAP="29" ID="Freemind_Link_1639986542" MODIFIED="1221803411500" TEXT="Needs accurate information" VSHIFT="19">
+<node COLOR="#990000" CREATED="1215515356484" HGAP="13" ID="Freemind_Link_1639986542" MODIFIED="1221956168250" TEXT="Needs accurate information" VSHIFT="25">
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1215515368453" ID="Freemind_Link_1667327265" MODIFIED="1215515375578" TEXT="Complete API documentation"/>
 <node COLOR="#111111" CREATED="1215515381750" ID="Freemind_Link_265163116" MODIFIED="1215515392453" TEXT="References to implemented algorithms"/>
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1215427407812" ID="Freemind_Link_1785791728" MODIFIED="1221715998937" POSITION="right" TEXT="User should always be in control">
+<node COLOR="#0033ff" CREATED="1215427407812" HGAP="5" ID="Freemind_Link_1785791728" MODIFIED="1221956140843" POSITION="right" TEXT="User should always be in control" VSHIFT="4">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 <node COLOR="#00b439" CREATED="1215427432546" ID="Freemind_Link_603280185" MODIFIED="1221715998937" TEXT="It&apos;s about the user, not the product">
