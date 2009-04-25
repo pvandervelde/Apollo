f4dcf0614b0b428e0d02b54a48a55d32c91ba612
Patrick <Patrick@SilverSurfer-PC>
2009-04-25 22:56:55 +1200

    Updating the build files.

--- modules/core/core_build.rb
+++ modules/core/core_build.rb
@@ -1,4 +1,18 @@
 # core_build.rb
 # Builds the core assemblies for Apollo
 
-# Only needs to define the paths to the different sources etc.
+require 'rake/clean'
+
+# Define which files to clean
+CLEAN = FileList[]
+
+# Define which files to delete in the clobber execution
+CLOBBER = FileList[]
+
+# Define the solution files
+SRC = FileList[]
+
+# Define the final BIN directory
+
+
+# Define the dependencies
--- templates/compile.rb
+++ templates/compile.rb
@@ -5,7 +5,7 @@ require 'dependencies'
 
 task :default => [:help]
 
-task :build => [:compile]
+task :build => [:compile, :copy]
 
 desc "Provides the help for the current task"
 task :help do
@@ -13,10 +13,16 @@ task :help do
 end
 
 desc "Runs the MsBuild script on the VS solutions"
-task :compile do
+task :compile => [:clobber] do
+	msbuild 
 end
 
 # Define the method that will run the MSBuild script
 def msbuild(solution)
 	sh "#{MSBUILD} #{solution}.sln"
+end
+
+desc ""
+task :copy do
+	# Copy the newly created binaries to the BIN folder
 end
