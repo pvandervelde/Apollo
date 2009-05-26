44efe73a1eb5650a6659cbff31653d5bbd75a344
Petrik <Petrik@SilverSurfer-PC>
2009-05-27 00:12:31 +1200

    Working on the ProgressTree control. This will probably lead to many parts of the common UI library being defined, which in turn leads to a large number of interfaces being defined in the core project. Need to do some specs first!

--- modules/ui/common/src/common/Properties/Resources.Designer.cs
+++ modules/ui/common/src/common/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Apollo.Ui.Common.Properties
-{
-
-
+namespace Apollo.Ui.Common.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,182 @@ namespace Apollo.Ui.Common.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Apollo.Ui.Common.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Pause_16x16 {
+            get {
+                object obj = ResourceManager.GetObject("Pause_16x16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Pause_24x24 {
+            get {
+                object obj = ResourceManager.GetObject("Pause_24x24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Pause_256x256 {
+            get {
+                object obj = ResourceManager.GetObject("Pause_256x256", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Pause_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("Pause_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Pause_48x48 {
+            get {
+                object obj = ResourceManager.GetObject("Pause_48x48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Play_16x16 {
+            get {
+                object obj = ResourceManager.GetObject("Play_16x16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Play_24x24 {
+            get {
+                object obj = ResourceManager.GetObject("Play_24x24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Play_256x256 {
+            get {
+                object obj = ResourceManager.GetObject("Play_256x256", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Play_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("Play_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Play_48x48 {
+            get {
+                object obj = ResourceManager.GetObject("Play_48x48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to More details....
+        /// </summary>
+        internal static string ProgressTreeNode_MoreDetails_Text {
+            get {
+                return ResourceManager.GetString("ProgressTreeNode_MoreDetails_Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Press to pause the data processing.
+        /// </summary>
+        internal static string ProgressTreeNode_Pause_Hint {
+            get {
+                return ResourceManager.GetString("ProgressTreeNode_Pause_Hint", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Press to start the data processing.
+        /// </summary>
+        internal static string ProgressTreeNode_Play_Hint {
+            get {
+                return ResourceManager.GetString("ProgressTreeNode_Play_Hint", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Press to stop the data processing.
+        /// </summary>
+        internal static string ProgressTreeNode_Stop_Hint {
+            get {
+                return ResourceManager.GetString("ProgressTreeNode_Stop_Hint", resourceCulture);
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Stop_16x16 {
+            get {
+                object obj = ResourceManager.GetObject("Stop_16x16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Stop_24x24 {
+            get {
+                object obj = ResourceManager.GetObject("Stop_24x24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Stop_256x256 {
+            get {
+                object obj = ResourceManager.GetObject("Stop_256x256", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Stop_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("Stop_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Stop_48x48 {
+            get {
+                object obj = ResourceManager.GetObject("Stop_48x48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
