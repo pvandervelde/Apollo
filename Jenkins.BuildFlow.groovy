f65a79850d22c1dfec31dcb5a19b1125f8124068
Petrik <petrikvandervelde@gmail.com>
2013-01-07 22:25:57 +1300

    Tweaks to the build flow

--- Jenkins.BuildFlow.groovy
+++ Jenkins.BuildFlow.groovy
@@ -9,7 +9,7 @@ testResultWorkspaceClone = prepareWorkspaceClone
 
 buildResult = hudson.model.Result.SUCCESS
 
-prepareWorkspaceBuild = build("apollo-Prepare-Workspace", WORKSPACE_ARCHIVE_NAME: prepareWorkspaceClone, ARCHIVE_LOCATION: archiveLocation)
+prepareWorkspaceBuild = build("Apollo-Prepare-Workspace", WORKSPACE_ARCHIVE_NAME: prepareWorkspaceClone, ARCHIVE_LOCATION: archiveLocation)
 buildResult = prepareWorkspaceBuild.build.result.combine(buildResult)
 
 out.println ''
@@ -21,15 +21,15 @@ guard {
     if (buildResult.isBetterOrEqualTo(hudson.model.Result.SUCCESS)) {
         parallel(
             { 
-                b = build("apollo-Analyze-Ccm", CLONED_WORKSPACE: prepareWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
+                b = build("Apollo-Analyze-Ccm", CLONED_WORKSPACE: prepareWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
                 buildResult = b.build.result.combine(buildResult)
             },
             { 
-                b = build("apollo-Analyze-SourceMonitor", CLONED_WORKSPACE: prepareWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
+                b = build("Apollo-Analyze-SourceMonitor", CLONED_WORKSPACE: prepareWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
                 buildResult = b.build.result.combine(buildResult)
             },
             { 
-                b = build("apollo-Build-Binaries", CLONED_WORKSPACE: prepareWorkspaceClone, WORKSPACE_ARCHIVE_NAME: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"]) 
+                b = build("Apollo-Build-Binaries", CLONED_WORKSPACE: prepareWorkspaceClone, WORKSPACE_ARCHIVE_NAME: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"]) 
                 buildResult = b.build.result.combine(buildResult)
                 
                 testResultWorkspaceClone = buildBinariesWorkspaceClone
@@ -44,19 +44,19 @@ guard {
     if (buildResult.isBetterOrEqualTo(hudson.model.Result.SUCCESS)) {
         parallel(
             {
-                b = build("apollo-Analyze-FxCop", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
+                b = build("Apollo-Analyze-FxCop", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
                 buildResult = b.build.result.combine(buildResult)
             },
             {
-                b = build("apollo-Analyze-Moma", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
+                b = build("Apollo-Analyze-Moma", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
                 buildResult = b.build.result.combine(buildResult)
             },
             {
-                b = build("apollo-Build-ApiDocumentation", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
+                b = build("Apollo-Build-ApiDocumentation", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
                 buildResult = b.build.result.combine(buildResult)
             },
             {
-                b = build("apollo-Test-UnitTest", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
+                b = build("Apollo-Test-UnitTest", CLONED_WORKSPACE: buildBinariesWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix) 
                 buildResult = b.build.result.combine(buildResult)
             },
         )
@@ -64,18 +64,18 @@ guard {
 
     packageMsiWorkspaceClone = "Apollo-PackageMsi-" + build.number
     if (buildResult.isBetterOrEqualTo(hudson.model.Result.SUCCESS)){
-        b = build("apollo-Package-Msi", CLONED_WORKSPACE: buildBinariesWorkspaceClone, WORKSPACE_ARCHIVE_NAME: packageMsiWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"]) 
+        b = build("Apollo-Package-Msi", CLONED_WORKSPACE: buildBinariesWorkspaceClone, WORKSPACE_ARCHIVE_NAME: packageMsiWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"]) 
         buildResult = b.build.result.combine(buildResult)
         
         testResultWorkspaceClone = packageMsiWorkspaceClone
     }
 
     if (buildResult.isBetterOrEqualTo(hudson.model.Result.SUCCESS)) {
-        b = build("apollo-Deploy-ToPackageRepository", CLONED_WORKSPACE: packageMsiWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"]) 
+        b = build("Apollo-Deploy-ToMsiRepository", CLONED_WORKSPACE: packageMsiWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"]) 
         buildResult = b.build.result.combine(buildResult)
     }
 } rescue {
-    b = build("apollo-Gather-BuildResults", CLONED_WORKSPACE: testResultWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix)
+    b = build("Apollo-Gather-BuildResults", CLONED_WORKSPACE: testResultWorkspaceClone, ARCHIVE_LOCATION: archiveLocation, BZR_REVISION: build.environment["BZR_REVISION"], TESTRESULTS_PREFIX: testResultPrefix)
     buildResult = b.build.result.combine(buildResult)
 }
 
