601cf62532dcd79cda6ec64face17a1c8b7ff344
Petrik van der Velde <petrikvandervelde@gmail.com>
2013-11-20 16:53:21 +1300

    Adding the GenericAutomationPeer and the GenericAutomationTabControl so that we can find the UI elements introduced by the TabControl style.

---
+++ src/ui.explorer/Controls/GenericAutomationTabControl.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="P. van der Velde">
+//     Copyright (c) P. van der Velde. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using Apollo.UI.Wpf.Automation;
+
+namespace Apollo.UI.Explorer.Controls
+{
+    /// <summary>
+    /// Defines a tab control which has special automation skills.
+    /// </summary>
+    public class GenericAutomationTabControl : TabControl
+    {
+        /// <summary>
+        /// Provides an appropriate <see cref="T:System.Windows.Automation.Peers.TabControlAutomationPeer"/> implementation for this 
+        /// control, as part of the WPF automation infrastructure.
+        /// </summary>
+        /// <returns>
+        /// The type-specific <see cref="T:System.Windows.Automation.Peers.AutomationPeer"/> implementation.
+        /// </returns>
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new GenericAutomationPeer(this);
+        }
+    }
+}
---
+++ src/ui.wpf/Automation/GenericAutomationPeer.cs
@@ -0,0 +1,67 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="P. van der Velde">
+//     Copyright (c) P. van der Velde. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Media;
+
+namespace Apollo.UI.Wpf.Automation
+{
+    /// <summary>
+    /// Defines a generic automation peer that can be used to find dynamically generated UI elements
+    /// for UI automation.
+    /// </summary>
+    public sealed class GenericAutomationPeer : UIElementAutomationPeer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericAutomationPeer"/> class.
+        /// </summary>
+        /// <param name="owner">The UI element that is the top level owner for this peer.</param>
+        public GenericAutomationPeer(UIElement owner)
+            : base(owner)
+        {
+        }
+
+        /// <summary>
+        /// Gets the collection of child elements of the <see cref="T:System.Windows.UIElement"/> that is associated with 
+        /// this <see cref="T:System.Windows.Automation.Peers.UIElementAutomationPeer"/>. This method is called by 
+        /// <see cref="M:System.Windows.Automation.Peers.AutomationPeer.GetChildren"/>.
+        /// </summary>
+        /// <returns>
+        /// A list of child <see cref="T:System.Windows.Automation.Peers.AutomationPeer"/> elements.
+        /// </returns>
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            var list = base.GetChildrenCore();
+            list.AddRange(GetChildPeers(Owner));
+            return list;
+        }
+
+        private List<AutomationPeer> GetChildPeers(UIElement element)
+        {
+            var list = new List<AutomationPeer>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i) as UIElement;
+                if (child != null)
+                {
+                    var childPeer = UIElementAutomationPeer.CreatePeerForElement(child);
+                    if (childPeer != null)
+                    {
+                        list.Add(childPeer);
+                    }
+                    else
+                    {
+                        list.AddRange(GetChildPeers(child));
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
