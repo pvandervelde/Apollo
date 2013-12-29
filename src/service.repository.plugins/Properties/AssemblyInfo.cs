40ca12c50b8db0ae29a69ac204674f6e8f70932b
Petrik van der Velde <petrikvandervelde@gmail.com>
2013-12-29 13:50:55 +1300

    Adding the service that will become the NuGet package source and the plugin repository.
    References ap-145

---
+++ src/service.repository.plugins/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="P. van der Velde">
+//     Copyright (c) P. van der Velde. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Apollo.Service.Repository")]
+[assembly: AssemblyDescription("Provides a single entry point for the plugin discovery and distribution services.")]
+[assembly: AssemblyProduct("Apollo.Service.Repository")]
+[assembly: AssemblyCulture("")]
+
+// Indicate that the assembly is CLS compliant.
+[assembly: CLSCompliant(true)]
+
+// Indicate that the neutral language resources are in the assembly.
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
+
